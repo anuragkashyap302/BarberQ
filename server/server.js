43462a7713b3f54bafcc551d5a9e145da5a8f13b
anuragkashyap302 <kumaranuragg68@gmail.com>
2026-01-11 15:22:19 +0530

    refjlkdsjf

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import dotenv from 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
@@ -9,27 +10,21 @@ import userRouter from './routes/userRoute.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
-
 connectDB();
 connectCloudinary();
-
-app.use(cors({
-  origin: '*',
-  methods: ['GET','POST','PUT','DELETE','PATCH','OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
+app.use(cors());
 app.use(express.json());
 
-// api endpoints
+// api endpints
 app.use('/api/admin', adminRouter);
 app.use('/api/barber', barberRouter);
 app.use('/api/user', userRouter);
-
+// localhost:4000/api/admin/add-barber
 app.get('/', (req, res) => {
   res.send('Api is Working Fine!');
 });
 
 app.listen(port, () => {
   console.log('Server is running on PORT:', port);
+  
 });
